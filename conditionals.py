b84141805177a37d4d412f5005ccef8edb813d19
Ernestoc14 <descreed34@hotmail.com>
2024-03-10 22:45:35 -0500

    Adding comments

--- conditionals.py
+++ conditionals.py
@@ -1,15 +1,18 @@
+# Choose one option 
 userOption = input("Choose between Rock, Paper or Scissors: ")
 
-
+# Convert the option to lower case
 def optionToLower(userOption):
   userOptionLower = userOption.lower()
   return userOptionLower
 
+# Generate a random option for the PC
 def pcOption():
   import random
   pcOption = random.choice(["rock", "paper", "scissors"])
   return pcOption
 
+# Check the option chosen by the user and the PC
 def checkOpcion(userOptionLower, pcOption):
   if userOptionLower == pcOption:
     return ""
@@ -26,6 +29,7 @@ def checkOpcion(userOptionLower, pcOption):
   else:
     return "User wins with {} vs {}".format(userOptionLower, pcOption)
   
+# Print the result
 returned = optionToLower(userOption)
 pcOption = pcOption()
 print(checkOpcion(returned, pcOption))
