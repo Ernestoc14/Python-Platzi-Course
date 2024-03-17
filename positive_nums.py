9db3b2dabdd3f75f64aadb46a905b590fd6ab941
Ernestoc14 <descreed34@hotmail.com>
2024-03-17 13:13:37 -0500

    Adding comments

--- positive_nums.py
+++ positive_nums.py
@@ -1,17 +1,14 @@
-# En este desafío, se te proporcionará una lista de números llamada my_list. 
-# Tu tarea es recorrer esta lista y utilizar un ciclo para seleccionar solo los números 
-# positivos. Luego, debes agregar estos números a una nueva lista llamada new_list. Al final del ciclo, 
-# debes imprimir los valores contenidos en new_list utilizando la función print.
-
-# Por ejemplo, si la lista es [1, -1, 2, -2, 3, -3, 4, -4], después de realizar las operaciones
-#  descritas, la lista new_list debería contener solo los números positivos, es decir, [1, 2, 3, 4].
+# Give it a list called my_list.
+# Loop throught list to select just the positive numbers
+# Then you have to add those numbers into a new list
+# Show values in the new list
 
 # Input: [1, -1, 2, -2, 3, -3, 4, -4]
 # Output: [1, 2, 3, 4]
 my_list = [1, -1, 2, -2, 3, -3, 4, -4]
 positive = []
 for num in my_list:
-  if num > 0:
-    positive.append(num)
+    if num > 0:
+        positive.append(num)
 
 print(positive)
