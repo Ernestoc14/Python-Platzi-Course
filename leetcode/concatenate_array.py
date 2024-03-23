22749c7e3964522946be75830662a9fc93add631
Ernestoc14 <descreed34@hotmail.com>
2024-03-23 15:58:27 -0500

    Concatenationof Array

---
+++ leetcode/concatenate_array.py
@@ -0,0 +1,28 @@
+# Given an integer array nums of length n, you want to create an
+# array ans of length 2n where ans[i] == nums[i] and
+# ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+# Specifically, ans is the concatenation of two nums arrays.
+# Return the array ans.
+
+# Example 1:
+# Input: nums = [1,2,1]
+# Output: [1,2,1,1,2,1]
+# Explanation: The array ans is formed as follows:
+# - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+# - ans = [1,2,1,1,2,1]
+
+# class Solution:
+#     def getConcatenation(self, nums: List[int]) -> List[int]:
+
+nums = [1, 7, 9, 0]
+ans = []
+for num in range(len(nums)):
+    # ans[num] == nums[num]
+    ans.append(nums[num])
+
+for num in range(len(nums)):
+    ans.append(nums[num])
+print(ans)
+
+# ans[i + n] == nums[i]
